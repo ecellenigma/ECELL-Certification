325322e3a5ac1e2e3284682cc8553572bc08f594
mannansaood <syed.mannan@outlook.com>
2025-01-26 12:21:02 +0530

    fixed firebase error

--- src/app/template-editor-example/page.tsx
+++ src/app/template-editor-example/page.tsx
@@ -9,12 +9,12 @@ import { addDoc, collection } from 'firebase/firestore';
 export default function TemplateEditorExample() {
   const [pdf, setPdf] = useState<File | null>(null);
   const [template, setTemplate] = useState<Template | null>(null);
+
   const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target && e.target.files) {
       setPdf(e.target.files[0]);
     }
   };
-
     const [csvFile, setCsvFile] = useState<File | null>(null);
     const [name,setName] = useState('')
     const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -31,8 +31,9 @@ export default function TemplateEditorExample() {
       complete: async (results) => {
         console.log('CSV data:', results.data);
         for (const row of results.data) {
+          console.log('row:', row)
           try {
-            const doc =await addDoc(collection(db, `${name}/`), row);
+            const doc =await addDoc(collection(db, `${name}`), row);
               console.log(doc);
           } catch (error) {
             console.error("Error adding document: ", error);
@@ -55,7 +56,7 @@ export default function TemplateEditorExample() {
     <h2 className="text-4xl text-center font-semibold text-neutral-800 dark:text-neutral-200 mb-8 font-dm-serif-display">
           Admin Panel
         </h2>
-      <input  className="w-1/3 my-6 border shadow-sm uppercase font-medium text-neutral-600 dark:bg-black placeholder:bg-black dark:placeholder:text-neutral-600 border-neutral-300 dark:border-neutral-800 bg-transparent rounded-md px-4 py-3 text-sm" type="text" placeholder='Enter Name' onChange={(e) => setName(e.target.value)} />
+      <input  className="w-1/3 my-6 border shadow-sm font-medium text-neutral-600 dark:bg-black placeholder:bg-black dark:placeholder:text-neutral-600 border-neutral-300 dark:border-neutral-800 bg-transparent rounded-md px-4 py-3 text-sm" type="text" placeholder='Enter Name' onChange={(e) => setName(e.target.value)} />
      <input className="block w-1/3 border shadow-sm  font-medium bg-neutral-black dark:bg-black dark:placeholder:text-neutral-600 px-4 py-3 text-sm text-neutral-600 placeholder-slate-950 dark:border-neutral-800 rounded-md file:bg-transparent file:text-neutral-600 border-neutral-300 file:border-0  cursor-pointer" type="file" accept=".csv" onChange={handleFileChange} />
      <button className="flex items-center justify-center max-w-xs gap-2 px-4 mt-4 py-2 w-full rounded-md shadow-sm bg-indigo-700 hover:shadow-md transition duration-150 ease active:scale-[99%] text-white" onClick={handleUpload}>Upload</button>
       {!pdf ? (
