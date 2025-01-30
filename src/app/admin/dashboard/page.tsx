bb491e6b37d35dae84478d02ea02f27522828d4b
arnav-kr <72879799+arnav-kr@users.noreply.github.com>
2025-01-30 22:46:55 +0530

    fix: ensure authed

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,12 +1,14 @@
-'use client'
-import { useState, useEffect } from 'react';
+"use client";
+import { useState, useEffect } from "react";
 import { getPrograms, deleteProgram } from "@/lib/firebase/firestore";
-import Link from 'next/link';
-import { Edit, Trash2 } from 'lucide-react';
+import Link from "next/link";
+import { Edit, LoaderCircleIcon, Trash2 } from "lucide-react";
+import { useAuth } from "@/providers/AuthContext";
 
 type Program = Array<string>;
 
 export default function Programs() {
+  const { user, authLoading } = useAuth();
   const [programs, setPrograms] = useState<Program>([]);
 
   useEffect(() => {
@@ -25,44 +27,63 @@ export default function Programs() {
   };
 
   return (
-    <div className="text-white p-6 flex flex-col items-center">
-      <h1 className="text-3xl font-bold mb-6">Programs</h1>
-      <div className="bg-gray-900 p-8 rounded-lg shadow-lg w-full max-w-4xl">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {programs.map((program, index) => (
-            <div key={index} className="relative bg-indigo-700 p-6 rounded-lg shadow-lg text-center">
-              <h2 className="text-2xl font-semibold mb-4">{program}</h2>
-              <div className="absolute top-2 left-2">
-                <Link href={`dashboard/programs/${program}/participants`} legacyBehavior>
-                  <a className="text-blue-300 hover:text-blue-500">
-                    <Edit className="h-5 w-5" />
-                  </a>
-                </Link>
-              </div>
-              <div className="absolute top-2 right-2">
-                <button
-                  className="text-red-300 hover:text-red-500"
-                  onClick={() => handleDelete(program)}
+    <>
+      {user && !authLoading ? (
+        <div className="text-white p-6 flex flex-col items-center">
+          <h1 className="text-3xl font-bold mb-6">Programs</h1>
+          <div className="bg-gray-900 p-8 rounded-lg shadow-lg w-full max-w-4xl">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+              {programs.map((program, index) => (
+                <div
+                  key={index}
+                  className="relative bg-indigo-700 p-6 rounded-lg shadow-lg text-center"
                 >
-                  <Trash2 className="h-5 w-5" />
-                </button>
-              </div>
-              <div className="flex justify-center space-x-4 mt-6">
-                <Link href={`dashboard/programs/${program}/template`} legacyBehavior>
-                  <a className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded">
-                    Details
+                  <h2 className="text-2xl font-semibold mb-4">{program}</h2>
+                  <div className="absolute top-2 left-2">
+                    <Link
+                      href={`dashboard/programs/${program}/participants`}
+                      legacyBehavior
+                    >
+                      <a className="text-blue-300 hover:text-blue-500">
+                        <Edit className="h-5 w-5" />
+                      </a>
+                    </Link>
+                  </div>
+                  <div className="absolute top-2 right-2">
+                    <button
+                      className="text-red-300 hover:text-red-500"
+                      onClick={() => handleDelete(program)}
+                    >
+                      <Trash2 className="h-5 w-5" />
+                    </button>
+                  </div>
+                  <div className="flex justify-center space-x-4 mt-6">
+                    <Link
+                      href={`dashboard/programs/${program}/template`}
+                      legacyBehavior
+                    >
+                      <a className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded">
+                        Details
+                      </a>
+                    </Link>
+                  </div>
+                </div>
+              ))}
+              <div className="bg-gray-800 p-6 rounded-lg shadow-lg flex items-center justify-center">
+                <Link href={`dashboard/create`} legacyBehavior>
+                  <a className="text-green-300 hover:text-green-500 text-4xl">
+                    +
                   </a>
                 </Link>
               </div>
             </div>
-          ))}
-          <div className="bg-gray-800 p-6 rounded-lg shadow-lg flex items-center justify-center">
-            <Link href={`dashboard/create`} legacyBehavior>
-              <a className="text-green-300 hover:text-green-500 text-4xl">+</a>
-            </Link>
           </div>
         </div>
-      </div>
-    </div>
+      ) : (
+        <div className="grid place-items-center p-8 gap-2 min-h-[100dvh]">
+          <LoaderCircleIcon className="size-16 stroke-indigo-700 animate-spin" />
+        </div>
+      )}
+    </>
   );
 }
--- src/app/admin/dashboard/programs/[program]/participants/page.tsx
+++ src/app/admin/dashboard/programs/[program]/participants/page.tsx
@@ -1,23 +1,46 @@
-'use client'
-import { useState, useEffect } from 'react';
-import { getParticipants, setParticipant, deleteParticipant, addParticipant } from "@/lib/firebase/firestore";
-import { Participant } from '@/types';
-import { useParams } from 'next/navigation';
-import { Edit, Trash2, PlusCircle, MinusCircle } from 'lucide-react';
+"use client";
+import { useState, useEffect } from "react";
+import {
+  getParticipants,
+  setParticipant,
+  deleteParticipant,
+  addParticipant,
+} from "@/lib/firebase/firestore";
+import { Participant } from "@/types";
+import { useParams } from "next/navigation";
+import {
+  Edit,
+  Trash2,
+  PlusCircle,
+  MinusCircle,
+  LoaderCircleIcon,
+} from "lucide-react";
+import { useAuth } from "@/providers/AuthContext";
 
 export default function ProgramParticipants() {
   const { program } = useParams();
+  const { user, authLoading } = useAuth();
+
   const [participants, setParticipants] = useState<Participant[]>([]);
-  const [selectedParticipant, setSelectedParticipant] = useState<Participant | null>(null);
-  const [newParticipant, setNewParticipant] = useState<Participant>({ id: '', name: '', rank: 0 });
+  const [selectedParticipant, setSelectedParticipant] =
+    useState<Participant | null>(null);
+  const [newParticipant, setNewParticipant] = useState<Participant>({
+    id: "",
+    name: "",
+    rank: 0,
+  });
   const [isAddingParticipant, setIsAddingParticipant] = useState(false);
-  const [selectedParticipants, setSelectedParticipants] = useState<Set<string>>(new Set());
+  const [selectedParticipants, setSelectedParticipants] = useState<Set<string>>(
+    new Set()
+  );
   const [selectAll, setSelectAll] = useState(false);
 
   useEffect(() => {
     async function fetchParticipants() {
       if (program) {
-        const participantsRef = await getParticipants(program as string) as Participant[];
+        const participantsRef = (await getParticipants(
+          program as string
+        )) as Participant[];
         setParticipants(participantsRef);
       }
     }
@@ -46,27 +69,35 @@ export default function ProgramParticipants() {
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     if (selectedParticipant) {
-      await setParticipant(program as string, selectedParticipant.id, selectedParticipant);
+      await setParticipant(
+        program as string,
+        selectedParticipant.id,
+        selectedParticipant
+      );
       setSelectedParticipant(null);
     } else {
       await addParticipant(program as string, newParticipant);
-      setNewParticipant({ id: '', name: '', rank: 0 });
+      setNewParticipant({ id: "", name: "", rank: 0 });
       setIsAddingParticipant(false);
     }
     // Refresh participants list
-    const participantsRef = await getParticipants(program as string) as Participant[];
+    const participantsRef = (await getParticipants(
+      program as string
+    )) as Participant[];
     setParticipants(participantsRef);
   };
 
   const handleDelete = async (participantId: string) => {
     await deleteParticipant(program as string, participantId);
     // Refresh participants list
-    const participantsRef = await getParticipants(program as string) as Participant[];
+    const participantsRef = (await getParticipants(
+      program as string
+    )) as Participant[];
     setParticipants(participantsRef);
   };
 
   const handleSelect = (participantId: string) => {
-    setSelectedParticipants(prevSelected => {
+    setSelectedParticipants((prevSelected) => {
       const newSelected = new Set(prevSelected);
       if (newSelected.has(participantId)) {
         newSelected.delete(participantId);
@@ -81,184 +112,216 @@ export default function ProgramParticipants() {
     if (selectAll) {
       setSelectedParticipants(new Set());
     } else {
-      const allParticipantIds = participants.map(participant => participant.id);
+      const allParticipantIds = participants.map(
+        (participant) => participant.id
+      );
       setSelectedParticipants(new Set(allParticipantIds));
     }
     setSelectAll(!selectAll);
   };
 
   const handleDeleteSelected = async () => {
-    const deletePromises = Array.from(selectedParticipants).map(participantId => deleteParticipant(program as string, participantId));
+    const deletePromises = Array.from(selectedParticipants).map(
+      (participantId) => deleteParticipant(program as string, participantId)
+    );
     await Promise.all(deletePromises);
     setSelectedParticipants(new Set());
     setSelectAll(false);
     // Refresh participants list
-    const participantsRef = await getParticipants(program as string) as Participant[];
+    const participantsRef = (await getParticipants(
+      program as string
+    )) as Participant[];
     setParticipants(participantsRef);
   };
 
   return (
-    <div className="text-white p-6 flex flex-col items-center">
-      <h1 className="text-3xl font-bold mb-6">Program: {program}</h1>
-      <div className="bg-gray-900 p-8 rounded-lg shadow-lg w-full max-w-4xl">
-        <table className="min-w-full bg-gray-900">
-          <thead>
-            <tr>
-              <th className="py-2 px-4 border-b border-gray-700 text-left">
-                <input
-                  type="checkbox"
-                  className="form-checkbox h-5 w-5 text-indigo-600"
-                  checked={selectAll}
-                  onChange={handleSelectAll}
-                />
-              </th>
-              <th className="py-2 px-4 border-b border-gray-700">ID</th>
-              <th className="py-2 px-4 border-b border-gray-700">Name</th>
-              <th className="py-2 px-4 border-b border-gray-700">Rank</th>
-              <th className="py-2 px-4 border-b border-gray-700">Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {participants.map(participant => (
-              <tr key={participant.id} className="text-center">
-                <td className="py-2 px-4 border-b border-gray-700 text-left">
+    <>
+      {user && !authLoading ? (
+        <div className="text-white p-6 flex flex-col items-center">
+          <h1 className="text-3xl font-bold mb-6">Program: {program}</h1>
+          <div className="bg-gray-900 p-8 rounded-lg shadow-lg w-full max-w-4xl">
+            <table className="min-w-full bg-gray-900">
+              <thead>
+                <tr>
+                  <th className="py-2 px-4 border-b border-gray-700 text-left">
+                    <input
+                      type="checkbox"
+                      className="form-checkbox h-5 w-5 text-indigo-600"
+                      checked={selectAll}
+                      onChange={handleSelectAll}
+                    />
+                  </th>
+                  <th className="py-2 px-4 border-b border-gray-700">ID</th>
+                  <th className="py-2 px-4 border-b border-gray-700">Name</th>
+                  <th className="py-2 px-4 border-b border-gray-700">Rank</th>
+                  <th className="py-2 px-4 border-b border-gray-700">
+                    Actions
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {participants.map((participant) => (
+                  <tr key={participant.id} className="text-center">
+                    <td className="py-2 px-4 border-b border-gray-700 text-left">
+                      <input
+                        type="checkbox"
+                        className="form-checkbox h-5 w-5 text-indigo-600"
+                        checked={selectedParticipants.has(participant.id)}
+                        onChange={() => handleSelect(participant.id)}
+                      />
+                    </td>
+                    <td className="py-2 px-4 border-b border-gray-700">
+                      {participant.id}
+                    </td>
+                    <td className="py-2 px-4 border-b border-gray-700">
+                      {participant.name}
+                    </td>
+                    <td className="py-2 px-4 border-b border-gray-700">
+                      {participant.rank}
+                    </td>
+                    <td className="py-2 px-4 border-b border-gray-700">
+                      <button
+                        className="text-blue-300 hover:text-blue-500 mx-2"
+                        onClick={() => handleParticipantClick(participant)}
+                      >
+                        <Edit className="h-5 w-5" />
+                      </button>
+                      <button
+                        className="text-red-300 hover:text-red-500 mx-2"
+                        onClick={() => handleDelete(participant.id)}
+                      >
+                        <Trash2 className="h-5 w-5" />
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <div className="flex flex-col items-center mt-4">
+              <button
+                className="bg-indigo-700 hover:bg-indigo-900 text-white font-bold py-2 px-4 rounded mb-4 w-full max-w-xs flex items-center justify-center"
+                onClick={() => {
+                  setIsAddingParticipant(!isAddingParticipant);
+                  setSelectedParticipant(null);
+                }}
+              >
+                {isAddingParticipant ? (
+                  <MinusCircle className="h-5 w-5 inline-block mr-2" />
+                ) : (
+                  <PlusCircle className="h-5 w-5 inline-block mr-2" />
+                )}
+                {isAddingParticipant ? "Cancel" : "Add Participant"}
+              </button>
+              {selectedParticipants.size > 0 && (
+                <button
+                  className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded w-full max-w-xs flex items-center justify-center"
+                  onClick={handleDeleteSelected}
+                >
+                  <Trash2 className="h-5 w-5 inline-block mr-2" />
+                  Delete Selected
+                </button>
+              )}
+            </div>
+          </div>
+
+          {selectedParticipant && (
+            <div className="bg-gray-800 p-6 rounded-lg shadow-lg mt-6 w-full max-w-md">
+              <h2 className="text-xl font-semibold mb-4">Edit Participant</h2>
+              <form onSubmit={handleSubmit}>
+                <div className="mb-4">
+                  <label className="block text-white mb-2" htmlFor="name">
+                    Name
+                  </label>
                   <input
-                    type="checkbox"
-                    className="form-checkbox h-5 w-5 text-indigo-600"
-                    checked={selectedParticipants.has(participant.id)}
-                    onChange={() => handleSelect(participant.id)}
+                    className="w-full p-2 rounded bg-gray-700 text-white"
+                    type="text"
+                    id="name"
+                    name="name"
+                    value={selectedParticipant.name}
+                    onChange={handleInputChange}
                   />
-                </td>
-                <td className="py-2 px-4 border-b border-gray-700">{participant.id}</td>
-                <td className="py-2 px-4 border-b border-gray-700">{participant.name}</td>
-                <td className="py-2 px-4 border-b border-gray-700">{participant.rank}</td>
-                <td className="py-2 px-4 border-b border-gray-700">
-                  <button
-                    className="text-blue-300 hover:text-blue-500 mx-2"
-                    onClick={() => handleParticipantClick(participant)}
-                  >
-                    <Edit className="h-5 w-5" />
-                  </button>
-                  <button
-                    className="text-red-300 hover:text-red-500 mx-2"
-                    onClick={() => handleDelete(participant.id)}
-                  >
-                    <Trash2 className="h-5 w-5" />
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        <div className="flex flex-col items-center mt-4">
-          <button
-            className="bg-indigo-700 hover:bg-indigo-900 text-white font-bold py-2 px-4 rounded mb-4 w-full max-w-xs flex items-center justify-center"
-            onClick={() => {
-              setIsAddingParticipant(!isAddingParticipant);
-              setSelectedParticipant(null);
-            }}
-          >
-            {isAddingParticipant ? (
-              <MinusCircle className="h-5 w-5 inline-block mr-2" />
-            ) : (
-              <PlusCircle className="h-5 w-5 inline-block mr-2" />
-            )}
-            {isAddingParticipant ? 'Cancel' : 'Add Participant'}
-          </button>
-          {selectedParticipants.size > 0 && (
-            <button
-              className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded w-full max-w-xs flex items-center justify-center"
-              onClick={handleDeleteSelected}
-            >
-              <Trash2 className="h-5 w-5 inline-block mr-2" />
-              Delete Selected
-            </button>
+                </div>
+                <div className="mb-4">
+                  <label className="block text-white mb-2" htmlFor="rank">
+                    Rank
+                  </label>
+                  <input
+                    className="w-full p-2 rounded bg-gray-700 text-white"
+                    type="text"
+                    id="rank"
+                    name="rank"
+                    value={selectedParticipant.rank}
+                    onChange={handleInputChange}
+                  />
+                </div>
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                  type="submit"
+                >
+                  Save
+                </button>
+              </form>
+            </div>
+          )}
+
+          {isAddingParticipant && (
+            <div className="bg-gray-800 p-6 rounded-lg shadow-lg mt-6 w-full max-w-md">
+              <h2 className="text-xl font-semibold mb-4">Add Participant</h2>
+              <form onSubmit={handleSubmit}>
+                <div className="mb-4">
+                  <label className="block text-white mb-2" htmlFor="id">
+                    ID
+                  </label>
+                  <input
+                    className="w-full p-2 rounded bg-gray-700 text-white"
+                    type="text"
+                    id="id"
+                    name="id"
+                    value={newParticipant.id}
+                    onChange={handleInputChange}
+                  />
+                </div>
+                <div className="mb-4">
+                  <label className="block text-white mb-2" htmlFor="name">
+                    Name
+                  </label>
+                  <input
+                    className="w-full p-2 rounded bg-gray-700 text-white"
+                    type="text"
+                    id="name"
+                    name="name"
+                    value={newParticipant.name}
+                    onChange={handleInputChange}
+                  />
+                </div>
+                <div className="mb-4">
+                  <label className="block text-white mb-2" htmlFor="rank">
+                    Rank
+                  </label>
+                  <input
+                    className="w-full p-2 rounded bg-gray-700 text-white"
+                    type="text"
+                    id="rank"
+                    name="rank"
+                    value={newParticipant.rank}
+                    onChange={handleInputChange}
+                  />
+                </div>
+                <button
+                  className="bg-indigo-700 hover:bg-indigo-900 text-white font-bold py-2 px-4 rounded w-full max-w-xs"
+                  type="submit"
+                >
+                  Add
+                </button>
+              </form>
+            </div>
           )}
         </div>
-      </div>
-
-      {selectedParticipant && (
-        <div className="bg-gray-800 p-6 rounded-lg shadow-lg mt-6 w-full max-w-md">
-          <h2 className="text-xl font-semibold mb-4">Edit Participant</h2>
-          <form onSubmit={handleSubmit}>
-            <div className="mb-4">
-              <label className="block text-white mb-2" htmlFor="name">Name</label>
-              <input
-                className="w-full p-2 rounded bg-gray-700 text-white"
-                type="text"
-                id="name"
-                name="name"
-                value={selectedParticipant.name}
-                onChange={handleInputChange}
-              />
-            </div>
-            <div className="mb-4">
-              <label className="block text-white mb-2" htmlFor="rank">Rank</label>
-              <input
-                className="w-full p-2 rounded bg-gray-700 text-white"
-                type="text"
-                id="rank"
-                name="rank"
-                value={selectedParticipant.rank}
-                onChange={handleInputChange}
-              />
-            </div>
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-              type="submit"
-            >
-              Save
-            </button>
-          </form>
+      ) : (
+        <div className="grid place-items-center p-8 gap-2 min-h-[100dvh]">
+          <LoaderCircleIcon className="size-16 stroke-indigo-700 animate-spin" />
         </div>
       )}
-
-      {isAddingParticipant && (
-        <div className="bg-gray-800 p-6 rounded-lg shadow-lg mt-6 w-full max-w-md">
-          <h2 className="text-xl font-semibold mb-4">Add Participant</h2>
-          <form onSubmit={handleSubmit}>
-            <div className="mb-4">
-              <label className="block text-white mb-2" htmlFor="id">ID</label>
-              <input
-                className="w-full p-2 rounded bg-gray-700 text-white"
-                type="text"
-                id="id"
-                name="id"
-                value={newParticipant.id}
-                onChange={handleInputChange}
-              />
-            </div>
-            <div className="mb-4">
-              <label className="block text-white mb-2" htmlFor="name">Name</label>
-              <input
-                className="w-full p-2 rounded bg-gray-700 text-white"
-                type="text"
-                id="name"
-                name="name"
-                value={newParticipant.name}
-                onChange={handleInputChange}
-              />
-            </div>
-            <div className="mb-4">
-              <label className="block text-white mb-2" htmlFor="rank">Rank</label>
-              <input
-                className="w-full p-2 rounded bg-gray-700 text-white"
-                type="text"
-                id="rank"
-                name="rank"
-                value={newParticipant.rank}
-                onChange={handleInputChange}
-              />
-            </div>
-            <button
-              className="bg-indigo-700 hover:bg-indigo-900 text-white font-bold py-2 px-4 rounded w-full max-w-xs"
-              type="submit"
-            >
-              Add
-            </button>
-          </form>
-        </div>
-      )}
-    </div>
+    </>
   );
 }
