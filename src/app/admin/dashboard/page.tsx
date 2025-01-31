80afc41267e84d4bbb6e13afc66f1a684a03904a
arnav-kr <72879799+arnav-kr@users.noreply.github.com>
2025-01-31 12:49:21 +0530

    fix: light mode theme

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -53,7 +53,7 @@ export default function Programs() {
               Logout
             </button>
           </div>
-          <div className="text-white p-6 md:p-8 flex flex-col items-center">
+          <div className="text-neutral-800 dark:text-white p-6 md:p-8 flex flex-col items-center">
             <h1 className="text-3xl font-bold mb-8 font-dm-serif-display">
               Programs
             </h1>
@@ -62,14 +62,14 @@ export default function Programs() {
                 {programs.map((program, index) => (
                   <div
                     key={index}
-                    className="border border-neutral-300 dark:border-neutral-800 p-2 rounded-lg flex flex-col justify-between gap-6"
+                    className="border border-neutral-200 dark:border-neutral-800 p-2 rounded-lg flex flex-col justify-between gap-6"
                   >
                     <div className="flex justify-between items-start gap-3 pl-1.5">
                       <span className="text-xl flex-1 mt-0.5 font-semibold">
                         {formatProgramName(program)}
                       </span>
                       <button
-                        className="p-2 max-h-fit rounded-md bg-red-500 hover:bg-red-600 dark:bg-red-700 dark:hover:bg-red-600 active:scale[99%] transition-transform duration-100 ease"
+                        className="p-2 max-h-fit rounded-md text-white bg-red-500 hover:bg-red-600 dark:bg-red-700 dark:hover:bg-red-600 active:scale[99%] transition-transform duration-100 ease"
                         onClick={() => handleDelete(program)}
                       >
                         <Trash2 className="size-4" />
@@ -78,14 +78,14 @@ export default function Programs() {
                     <div className="flex flex-col justify-center gap-2 p-1">
                       <Link
                         href={`dashboard/programs/${program}/participants`}
-                        className="w-full px-3 py-2 text-sm font-medium flex justify-center items-center gap-2 rounded-md bg-neutral-200 dark:bg-neutral-800 hover:bg-neutral-400 dark:hover:bg-neutral-700 cursor-pointer transition-transform active:scale-[99%] duration-100 ease"
+                        className="w-full px-3 py-2 text-sm font-medium flex justify-center items-center gap-2 rounded-md bg-none border border-neutral-200 hover:bg-neutral-200 dark:hover:bg-neutral-700 cursor-pointer transition active:scale-[99%] duration-100 ease"
                       >
                         <Users className="size-4" />
                         <span>Edit Participants</span>
                       </Link>
                       <Link
                         href={`dashboard/programs/${program}/template`}
-                        className="w-full px-3 py-2 text-sm font-medium flex justify-center items-center gap-2 rounded-md bg-neutral-200 dark:bg-neutral-800 hover:bg-neutral-400 dark:hover:bg-neutral-700 cursor-pointer transition-transform active:scale-[99%] duration-100 ease"
+                        className="w-full px-3 py-2 text-sm font-medium flex justify-center items-center gap-2 rounded-md bg-none border border-neutral-200 hover:bg-neutral-200 dark:hover:bg-neutral-700 cursor-pointer transition active:scale-[99%] duration-100 ease"
                       >
                         <LayoutTemplate className="size-4" />
                         <span>Edit Template</span>
@@ -95,7 +95,7 @@ export default function Programs() {
                 ))}
                 <Link
                   href={`dashboard/create`}
-                  className="border p-6 min-h-36 md:min-h-none border-neutral-300 dark:border-neutral-800 hover:bg-neutral-200 dark:hover:bg-neutral-900 rounded-lg flex flex-col justify-center items-center gap-2"
+                  className="border p-6 min-h-36 md:min-h-none border-neutral-200 dark:border-neutral-800 hover:bg-neutral-200 dark:hover:bg-neutral-900 rounded-lg flex flex-col justify-center items-center gap-2 transition duration-100 ease"
                 >
                   <Plus className="size-7" />
                   <span className="text-lg font-semibold">
